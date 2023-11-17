7be16ff9379b1e65e71f09e1015618b80861bf8c
selasiNyakpoh <selasinyakpoh@gmail.com>
2023-11-17 21:00:37 +0000

    my first project

---
+++ light.js
@@ -0,0 +1,14 @@
+const switchBtn = document.querySelector('.switchBtn input')
+//console.log(switchBtn)
+
+switchBtn.addEventListener('click', () => {
+ let img = document.querySelector('#demo');
+ 
+ if(img.src.match('bulbon.png')){
+   img.src = 'bulboff.png'
+
+ }  else {
+   img.src = 'bulbon.png'
+
+ }
+})
